773a0f08e958548174e278d330e3503e207aa6b2
xung <nmdtruong1832004@gmail.com>
2024-03-15 19:59:11 +0700

    demo login

--- QuanLyPhongKham/Form1.Designer.cs
+++ QuanLyPhongKham/Form1.Designer.cs
@@ -32,62 +32,47 @@ namespace QuanLyPhongKham
             System.Windows.Forms.Button button4;
             System.Windows.Forms.Label label1;
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
             this.linkLabel2 = new System.Windows.Forms.LinkLabel();
             this.label2 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.linkLabel3 = new System.Windows.Forms.LinkLabel();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
-            this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.cbkShowPassword = new System.Windows.Forms.CheckBox();
+            this.linkLabel3 = new System.Windows.Forms.LinkLabel();
+            this.tbxPassword = new System.Windows.Forms.TextBox();
+            this.tbxUsername = new System.Windows.Forms.TextBox();
             button4 = new System.Windows.Forms.Button();
             label1 = new System.Windows.Forms.Label();
-            this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // panel1
+            // button4
             // 
-            this.panel1.Controls.Add(this.button3);
-            this.panel1.Controls.Add(this.button2);
-            this.panel1.Controls.Add(this.button1);
-            this.panel1.Location = new System.Drawing.Point(-7, -5);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1437, 41);
-            this.panel1.TabIndex = 0;
+            button4.BackColor = System.Drawing.Color.LightSteelBlue;
+            button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            button4.Cursor = System.Windows.Forms.Cursors.SizeNESW;
+            button4.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            button4.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            button4.Location = new System.Drawing.Point(119, 347);
+            button4.Name = "button4";
+            button4.Size = new System.Drawing.Size(430, 56);
+            button4.TabIndex = 12;
+            button4.Text = "Đăng nhập";
+            button4.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += new System.EventHandler(this.button4_Click);
             // 
-            // button3
+            // label1
             // 
-            this.button3.Location = new System.Drawing.Point(1322, 6);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(33, 35);
-            this.button3.TabIndex = 1;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(1349, 6);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(34, 35);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(1383, 5);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(34, 36);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            label1.AutoSize = true;
+            label1.BackColor = System.Drawing.Color.Transparent;
+            label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            label1.Location = new System.Drawing.Point(125, 425);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(184, 20);
+            label1.TabIndex = 15;
+            label1.Text = "Bạn chưa có tài khoản?";
             // 
             // linkLabel2
             // 
@@ -108,7 +93,7 @@ namespace QuanLyPhongKham
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.label2.Location = new System.Drawing.Point(201, 46);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(243, 52);
+            this.label2.Size = new System.Drawing.Size(241, 51);
             this.label2.TabIndex = 9;
             this.label2.Text = "Đăng nhập";
             this.label2.Click += new System.EventHandler(this.label2_Click);
@@ -120,28 +105,64 @@ namespace QuanLyPhongKham
             this.panel2.Controls.Add(this.label3);
             this.panel2.Controls.Add(this.linkLabel1);
             this.panel2.Controls.Add(label1);
-            this.panel2.Controls.Add(this.checkBox1);
+            this.panel2.Controls.Add(this.cbkShowPassword);
             this.panel2.Controls.Add(this.linkLabel3);
             this.panel2.Controls.Add(button4);
-            this.panel2.Controls.Add(this.textBox2);
-            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.tbxPassword);
+            this.panel2.Controls.Add(this.tbxUsername);
             this.panel2.Controls.Add(this.label2);
             this.panel2.Location = new System.Drawing.Point(799, 267);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(577, 493);
             this.panel2.TabIndex = 10;
             // 
-            // checkBox1
+            // label4
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.BackColor = System.Drawing.Color.Transparent;
-            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.checkBox1.Location = new System.Drawing.Point(405, 309);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(144, 22);
-            this.checkBox1.TabIndex = 14;
-            this.checkBox1.Text = "Hiển thị mật khẩu";
-            this.checkBox1.UseVisualStyleBackColor = false;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label4.Location = new System.Drawing.Point(14, 247);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 25);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Mật khẩu";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label3.Location = new System.Drawing.Point(14, 162);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 25);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Tài khoản";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.linkLabel1.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
+            this.linkLabel1.LinkColor = System.Drawing.Color.Black;
+            this.linkLabel1.Location = new System.Drawing.Point(316, 425);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(103, 18);
+            this.linkLabel1.TabIndex = 16;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Tạo Tài khoản";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // cbkShowPassword
+            // 
+            this.cbkShowPassword.AutoSize = true;
+            this.cbkShowPassword.BackColor = System.Drawing.Color.Transparent;
+            this.cbkShowPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.cbkShowPassword.Location = new System.Drawing.Point(405, 309);
+            this.cbkShowPassword.Name = "cbkShowPassword";
+            this.cbkShowPassword.Size = new System.Drawing.Size(144, 22);
+            this.cbkShowPassword.TabIndex = 14;
+            this.cbkShowPassword.Text = "Hiển thị mật khẩu";
+            this.cbkShowPassword.UseVisualStyleBackColor = false;
+            this.cbkShowPassword.CheckedChanged += new System.EventHandler(this.cbkShowPassword_CheckedChanged);
             // 
             // linkLabel3
             // 
@@ -157,99 +178,41 @@ namespace QuanLyPhongKham
             this.linkLabel3.TabStop = true;
             this.linkLabel3.Text = "Quên Mật Khẩu";
             // 
-            // button4
+            // tbxPassword
             // 
-            button4.BackColor = System.Drawing.Color.LightSteelBlue;
-            button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            button4.Cursor = System.Windows.Forms.Cursors.SizeNESW;
-            button4.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            button4.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            button4.Location = new System.Drawing.Point(119, 347);
-            button4.Name = "button4";
-            button4.Size = new System.Drawing.Size(430, 56);
-            button4.TabIndex = 12;
-            button4.Text = "Đăng nhập";
-            button4.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
-            button4.UseVisualStyleBackColor = false;
+            this.tbxPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxPassword.Location = new System.Drawing.Point(119, 236);
+            this.tbxPassword.Multiline = true;
+            this.tbxPassword.Name = "tbxPassword";
+            this.tbxPassword.PasswordChar = '*';
+            this.tbxPassword.Size = new System.Drawing.Size(433, 45);
+            this.tbxPassword.TabIndex = 11;
             // 
-            // textBox2
+            // tbxUsername
             // 
-            this.textBox2.Location = new System.Drawing.Point(119, 236);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(430, 56);
-            this.textBox2.TabIndex = 11;
-            // 
-            // textBox1
-            // 
-            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox1.Location = new System.Drawing.Point(119, 141);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(430, 55);
-            this.textBox1.TabIndex = 10;
-            // 
-            // linkLabel1
-            // 
-            this.linkLabel1.AutoSize = true;
-            this.linkLabel1.BackColor = System.Drawing.Color.Transparent;
-            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.linkLabel1.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
-            this.linkLabel1.LinkColor = System.Drawing.Color.Black;
-            this.linkLabel1.Location = new System.Drawing.Point(316, 425);
-            this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(103, 18);
-            this.linkLabel1.TabIndex = 16;
-            this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = "Tạo Tài khoản";
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.BackColor = System.Drawing.Color.Transparent;
-            label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            label1.Location = new System.Drawing.Point(125, 425);
-            label1.Name = "label1";
-            label1.Size = new System.Drawing.Size(184, 20);
-            label1.TabIndex = 15;
-            label1.Text = "Bạn chưa có tài khoản?";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.label3.Location = new System.Drawing.Point(14, 162);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(99, 25);
-            this.label3.TabIndex = 17;
-            this.label3.Text = "Tài khoản";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.label4.Location = new System.Drawing.Point(14, 247);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(93, 25);
-            this.label4.TabIndex = 18;
-            this.label4.Text = "Mật khẩu";
+            this.tbxUsername.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbxUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxUsername.Location = new System.Drawing.Point(119, 141);
+            this.tbxUsername.Multiline = true;
+            this.tbxUsername.Name = "tbxUsername";
+            this.tbxUsername.Size = new System.Drawing.Size(433, 45);
+            this.tbxUsername.TabIndex = 10;
+            this.tbxUsername.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1422, 977);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.linkLabel2);
-            this.Controls.Add(this.panel1);
             this.DoubleBuffered = true;
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            this.panel1.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
             this.ResumeLayout(false);
@@ -258,19 +221,14 @@ namespace QuanLyPhongKham
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.LinkLabel linkLabel2;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.LinkLabel linkLabel1;
-        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox cbkShowPassword;
         private System.Windows.Forms.LinkLabel linkLabel3;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox tbxPassword;
+        private System.Windows.Forms.TextBox tbxUsername;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
     }
